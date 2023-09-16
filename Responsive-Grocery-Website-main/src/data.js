b49a6d986bf2aed34aa19699adff7f4ee2b67546
MADHAN KUMAR M <123907939+the-madhankumar@users.noreply.github.com>
2023-09-16 19:37:36 +0530

    Add files via upload

---
+++ Responsive-Grocery-Website-main/src/data.js
@@ -0,0 +1,86 @@
+let shopItemsData = [
+    {
+        id:"id1",
+        name:"Lemons",
+        price: 4,
+        desc: "$2.00/lb, 2lb bag",
+        img: "images/lemon2.jpg"
+    },
+    {
+        id:"id2",
+        name:"Oranges",
+        price: 5,
+        desc: "$1.25/lb, 4lb bag",
+        img: "images/orange2.jpg"
+    },
+    {
+        id:"id3",
+        name:"Tomatoes",
+        price: 3,
+        desc: "$1.50/lb, 2lb bag",
+        img: "images/tomato.jpg"
+    },
+    {
+        id:"id4",
+        name:"Kiwis",
+        price: 5,
+        desc: "$2.50/lb, 2lb box",
+        img: "images/kiwi2.jpg"
+    },
+    {
+        id:"id5",
+        name:"Apples",
+        price: 4,
+        desc: "$2.00/lb, 2lb bag",
+        img: "images/apple.jpg"
+    },
+    {
+        id:"id6",
+        name:"Strawberries",
+        price: 4,
+        desc: "$2/lb, 2lb box",
+        img: "images/strawberry2.jpg"
+    },
+    {
+        id:"id7",
+        name:"Pomegranates",
+        price: 3,
+        desc: "$3.00/lb, 1lb bag",
+        img: "images/pom.jpg"
+    },
+    {
+        id:"id8",
+        name:"Mangoes",
+        price: 5,
+        desc: "$2.50/lb, 2lb box",
+        img: "images/mango2.jpg"
+    },
+    {
+        id:"id9",
+        name:"Avocados",
+        price: 4,
+        desc: "$4.00/lb, 1lb bag",
+        img: "images/avocado3.jpg"
+    },
+    {
+        id:"id10",
+        name:"Cherries",
+        price: 3,
+        desc: "$1.50/lb, 2lb box",
+        img: "images/cherry.jpg"
+    },
+    {
+        id:"id11",
+        name:"Watermelon",
+        price: 4,
+        desc: "$2/lb, 2lb item",
+        img: "images/watermelon.jpg"
+    },
+    {
+        id:"id12",
+        name:"Pineapples",
+        price: 2,
+        desc: "$2.00/lb, 2lb item",
+        img: "images/pineapple.jpg"
+    }
+];
